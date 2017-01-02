d9cf1c9fad8e66db30639564d164bd71cd11ca90
Chā <chavis.clark@gmail.com>
2017-01-02 18:08:39 -0500

    set up mongoose user schema

---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//Define model
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true
+  },
+  password: String
+});
+
+//Create the model class
+const UserModelClass = mongoose.model('user', userSchema);
+
+//Export model
+
+module.exports = UserModelClass
