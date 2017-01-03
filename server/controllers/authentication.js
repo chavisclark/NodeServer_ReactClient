68faba013e821cd82fc873eab173837595879890
Chā <chavis.clark@gmail.com>
2017-01-02 19:42:00 -0500

    wire up auth controller and signup function

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,27 @@
 exports.signup = function(req, res, next) {
-  res.send({success: true})
+  //See if a user with the given email exists
+  const email = req.body.email;
+  const password = req.body.pass;
+
+  User.findOne({ email: email}, function(err, existingUser) {
+    if (err) { return next(err) }
+    //If a user with email exisits, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' })
+    }
+  
+    //If a user with email does NOT exisit, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err) }
+
+      //Respond to requst indicating the user has been created
+      res.json({success: true})
+    });
+
+  });
 }
