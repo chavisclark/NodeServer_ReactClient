69e16aecc7049532b2684c8e8e247bf40e536ec2
Chā <chavis.clark@gmail.com>
2017-01-11 16:06:31 -0500

    fix typo

--- src/components/auth/signout.js
+++ src/components/auth/signout.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import * as actions from '../../actions/types';
+import * as actions from '../../actions';
 
 
 class SignOut extends Component {
