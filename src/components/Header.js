98756d16cee59df2ca17e8206ebe14cf12b7ee96
Chā <chavis.clark@gmail.com>
2017-01-11 16:05:53 -0500

    built signout component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router';
 
 class Header extends Component {
+  renderLinks() {
+    if(this.props.authenticated) {
+      //Show a link to sign out
+      return 
+        <li className="nav-item">
+          <Link className="nav-link" to="/signout">Sign Out</Link>
+        </li>
+    } else {
+      //Show a link to sign in / sign up
+      return [
+         <li key="1" className="nav-item">
+            <Link to="/signin" className="nav-link">Sign In</Link>
+        </li>,
+         <li key="2" className="nav-item">
+            <Link to="/signup" className="nav-link">Sign Up</Link>
+        </li>
+
+      ]    
+    }
+  }
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link className="navbar-brand">NSRC</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign in
-          </li>
+          
+            {this.renderLinks()}
+
         </ul>
       </nav>
     )
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated
+  };
+}
+
+export default connect(mapStateToProps)(Header);
