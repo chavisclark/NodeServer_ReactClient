9410d020e595c0466e43ad43481bfd4b2099475a
Chā <chavis.clark@gmail.com>
2017-01-14 15:20:16 -0500

    fixed server typo

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -15,7 +15,7 @@ exports.signin = function(req, res, next) {
 
 }
 
-exports.signup = function(err, req, res, next) {
+exports.signup = function(req, res, next) {
   //See if a user with the given email exists
   const email = req.body.email;
   const password = req.body.password;
@@ -28,7 +28,7 @@ exports.signup = function(err, req, res, next) {
     if (err) { return next(err) }
     //If a user with email exisits, return an error
     if (existingUser) {
-      return res.status(422).json({ error: 'Email is in use' })
+      return res.status(422).send({ error: 'Email is in use' })
     }
   
     //If a user with email does NOT exisit, create and save user record
