5304494b0ed9068a36d08dcc73d92be4c0daa746
Chā <chavis.clark@gmail.com>
2017-01-24 23:42:58 -0500

    text tweak

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -30,11 +30,11 @@ class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navBarID" aria-controls="navBarID" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <a className="navbar-brand" href="/">Express & React</a>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="collapse navbar-collapse" id="navBarID">
           <div className="nav navbar-nav">
             
               {this.renderLinks()}
